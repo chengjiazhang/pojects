0ee291a1482532f69962bc6ac725f7facb1f94b9
zhangchengjia <changjiazhangnet@163.com>
2019-10-10 11:06:32 +0800

    000

--- java/sql/zcj.sql
+++ java/sql/zcj.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 09/10/2019 19:59:48
+ Date: 10/10/2019 11:05:18
 */
 
 SET NAMES utf8mb4;
@@ -31,7 +31,7 @@ CREATE TABLE `resale` (
   `saleid` int(11) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=gb2312 PACK_KEYS=1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=gb2312 PACK_KEYS=1;
 
 -- ----------------------------
 -- Records of resale
@@ -63,6 +63,36 @@ INSERT INTO `resale` VALUES ('可乐', 1, '2019-09-23 00:00:00', '1', 2, 'lilei'
 INSERT INTO `resale` VALUES ('可乐', 1, '2019-09-23 00:00:00', '1', 2, 'lilei', 1, 27);
 INSERT INTO `resale` VALUES ('可乐', 1, '2019-09-23 00:00:00', '1', 2, 'lilei', 1, 28);
 INSERT INTO `resale` VALUES ('可乐', 1, '2019-09-23 00:00:00', '1', 2, 'lilei', 1, 29);
+INSERT INTO `resale` VALUES ('可乐', 1, '2019-10-10 00:00:00', '1', 2, 'lilei', 1, 30);
+INSERT INTO `resale` VALUES ('可乐', 1, '2019-10-10 00:00:00', '1', 2, 'lilei', 1, 31);
+INSERT INTO `resale` VALUES ('可乐', 1, '2019-10-10 00:00:00', '1', 2, 'lilei', 1, 32);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sale
+-- ----------------------------
+DROP TABLE IF EXISTS `sale`;
+CREATE TABLE `sale` (
+  `commodityname` varchar(45) NOT NULL,
+  `commodityid` int(10) unsigned NOT NULL,
+  `sellTime` datetime NOT NULL,
+  `number` varchar(45) NOT NULL,
+  `money` double NOT NULL,
+  `operator` varchar(45) NOT NULL,
+  `saleid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=gb2312 PACK_KEYS=1;
+
+-- ----------------------------
+-- Records of sale
+-- ----------------------------
+BEGIN;
+INSERT INTO `sale` VALUES ('可乐', 1, '2019-10-10 00:00:00', '1', 2, 'lilei', 1, 1);
+INSERT INTO `sale` VALUES ('雪碧', 2, '2019-10-10 00:00:00', '1', 3, 'lili', 2, 2);
+INSERT INTO `sale` VALUES ('橙汁', 3, '2019-10-10 00:00:00', '1', 3.5, 'ko', 3, 3);
+INSERT INTO `sale` VALUES ('酸奶', 4, '2019-10-10 00:00:00', '1', 5, 'zz', 4, 4);
+INSERT INTO `sale` VALUES ('运动饮料', 5, '2019-10-10 00:00:00', '1', 4, 'll', 5, 5);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
